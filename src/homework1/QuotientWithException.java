5a290fe39bdaa1a78cb524c265e45f546a84042c
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-02-17 03:46:48 -0800

    add QuotientWithException

---
+++ src/homework1/QuotientWithException.java
@@ -0,0 +1,29 @@
+package homework1;
+
+import java.util.Scanner;
+
+public class QuotientWithException {
+    public static int quotient(int number1, int number2) {
+        if (number2 == 0)
+            throw new ArithmeticException("Divisor cannot be zero");
+
+        return number1 / number2;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter two integers: ");
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
+
+        try {
+
+        }catch (ArithmeticException ex) {
+            System.out.println("Exception: an integer " +
+                    "cannot be divided by zero ");
+        }
+
+        System.out.println("Execution continues ...");
+    }
+}
